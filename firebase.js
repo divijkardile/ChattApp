5c0a2727a7f1bcd94628862ef3c05ade25f00f3e
Divij Kardile <31166128+divijkardile@users.noreply.github.com>
2020-11-15 23:57:04 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,7 @@ import firebase from 'firebase';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBAD--gsagiqdv26S_kPp7BJDurqNI6WOY",
-    authDomain: "fir-con-5932e.firebaseapp.com",
-    databaseURL: "https://fir-con-5932e.firebaseio.com",
-    projectId: "fir-con-5932e",
-    storageBucket: "fir-con-5932e.appspot.com",
-    messagingSenderId: "385501736986",
-    appId: "1:385501736986:web:f414d698b0a8bf6619e1a3",
-    measurementId: "G-LQ9PDK4END"
+    //paste your config file here...
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
